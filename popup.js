89f3a84cc5c0a1005962267bb0b2b24ddba5a1bd
Norman <thenormanmu@gmail.com>
2015-10-10 16:22:46 -0700

    buttons work

--- popup.js
+++ popup.js
@@ -1,10 +1,15 @@
 document.getElementById("login-button").addEventListener("click", function() {
+    document.getElementById("keybase-login").style.display = "none";
+    document.getElementById("message-input").style.display = "block";
     var username = document.getElementById("login-username").value;
     var password = document.getElementById("login-password").value;
-    var xhr = new XMLHttpRequest();
-
-    xhr.open("GET", "https://keybase.io/_/api/1.0/getsalt.json?email_or_username=" + username, false);
-    xhr.send();
-
-    var login_salt = xhr.response;
+    console.log(username);
+    console.log(password);
+    // var xhr = new XMLHttpRequest();
+    //
+    // xhr.open("GET", "https://keybase.io/_/api/1.0/getsalt.json?email_or_username=" + username, false);
+    // xhr.send();
+    //
+    // var login_salt = xhr.response;
+    // confirm(login_salt.session);
 });
